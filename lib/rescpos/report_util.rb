39d17c1a1458b61f4b0beaba96851eef85bdc80e
Damian Serrano Thode <dsthode@gmail.com>
2015-05-31 22:12:07 +0200

    fixed tabbing

--- lib/rescpos/report_util.rb
+++ lib/rescpos/report_util.rb
@@ -1,11 +1,10 @@
 module Rescpos
-  module ReportUtil
-    FONT_NORMAL = "\x00"
-    FONT_BIG = "\x11"
-    ALIGN_C = "\x01"
-    ALIGN_L = "\x00"
-    ALIGN_R = "\x02"
-
+	module ReportUtil
+		FONT_NORMAL = "\x00"
+		FONT_BIG = "\x11"
+		ALIGN_C = "\x01"
+		ALIGN_L = "\x00"
+		ALIGN_R = "\x02"
 		BARCODE_UPCA = 65
 		BARCODE_UPCE = 66
 		BARCODE_EAN13 = 67
@@ -16,90 +15,90 @@ module Rescpos
 		BARCODE_CODE93 = 72
 		BARCODE_CODE128 = 73
 
-    def single_splitline
-      text("-" * 42, :font_size => FONT_NORMAL)
-    end
+		def single_splitline
+			text("-" * 42, :font_size => FONT_NORMAL)
+		end
 
-    def double_splitline
-      text("=" * 42, :font_size => FONT_NORMAL)
-    end
+		def double_splitline
+			text("=" * 42, :font_size => FONT_NORMAL)
+		end
 
-    def underline(number)
-      text("_" * number, :font_size => FONT_NORMAL)
-    end
+		def underline(number)
+			text("_" * number, :font_size => FONT_NORMAL)
+		end
 
-    def chinese(chinese)
-      text = Iconv.iconv("GBK//IGNORE","UTF-8//IGNORE",chinese)
-      text[0]
-    end
+		def chinese(chinese)
+			text = Iconv.iconv("GBK//IGNORE","UTF-8//IGNORE",chinese)
+			text[0]
+		end
 
-    def text(txt, options = {}) 
-      font_size = options[:font_size] || FONT_NORMAL
-      formatted_text = ''
-      formatted_text << fontsize(font_size)
-      formatted_text << grayscale(options[:gray]) if options[:gray]
-      formatted_text << align(options[:align_type]) if options[:align_type]
-      formatted_text << txt if txt
-    end
-    
-    def fontsize(size)
-      "\x1d\x21" << size.to_s
-    end
+		def text(txt, options = {}) 
+			font_size = options[:font_size] || FONT_NORMAL
+			formatted_text = ''
+			formatted_text << fontsize(font_size)
+			formatted_text << grayscale(options[:gray]) if options[:gray]
+			formatted_text << align(options[:align_type]) if options[:align_type]
+			formatted_text << txt if txt
+		end
+		
+		def fontsize(size)
+			"\x1d\x21" << size.to_s
+		end
 
-    def grayscale(value)
-      "\x1b\x6d" << ascii(value)
-    end
+		def grayscale(value)
+			"\x1b\x6d" << ascii(value)
+		end
 
-    def key_value(label, value)
-      "#{label}: #{value}"
-    end
+		def key_value(label, value)
+			"#{label}: #{value}"
+		end
 
-    def align(type)
-      "\x1b\x61" << type.to_s
-    end
-    
-    def table(data)
-      table = Rescpos::Table.new(data)
-      yield table
-      # FIXME move the following logic into Rescpos::Table
-      command = "\x1b\x44"
-      table.positions.each do |position|
-        command << position.chr
-      end
-      command << "\x00"
-      if table.headers
-        table.headers.each do |header|
-          command << header << "\x09" 
-        end
-        command << "\n"
-      end
-      table.data.each do |item|
-        if item.is_a? Array
-          command = command + item.join("\x09") + (table.data.last == item ? "" : "\n")
-        else
-          table.keys.each do |key|
-            if item.is_a? Hash
-              command << "#{item[key]}\x09"
-            else
-              command << "#{item.send(key)}\x09"
-            end
-          end
-          if table.data.last == item
-            return command
-          end
-          command << "\n"
-        end
-      end
-      command
-    end
+		def align(type)
+			"\x1b\x61" << type.to_s
+		end
+		
+		def table(data)
+			table = Rescpos::Table.new(data)
+			yield table
+			# FIXME move the following logic into Rescpos::Table
+			command = "\x1b\x44"
+			table.positions.each do |position|
+				command << position.chr
+			end
+			command << "\x00"
+			if table.headers
+				table.headers.each do |header|
+					command << header << "\x09" 
+				end
+				command << "\n"
+			end
+			table.data.each do |item|
+				if item.is_a? Array
+					command = command + item.join("\x09") + (table.data.last == item ? "" : "\n")
+				else
+					table.keys.each do |key|
+						if item.is_a? Hash
+							command << "#{item[key]}\x09"
+						else
+							command << "#{item.send(key)}\x09"
+						end
+					end
+					if table.data.last == item
+						return command
+					end
+					command << "\n"
+				end
+			end
+			command
+		end
 
-    def horizontal_tab
-      "\x09"
-    end
-    
-    def ascii(value)
-      value.to_s.unpack('U')[0].to_s(16)
-    end
+		def horizontal_tab
+			"\x09"
+		end
+		
+		def ascii(value)
+			value.to_s.unpack('U')[0].to_s(16)
+		end
 
 		def full_cut
 			"\n\x1d\x56#{65.chr}\x0c"
@@ -147,7 +146,7 @@ module Rescpos
 						k = image_h + l
 					end
 					p = 10 + k * (bitmap_w >> 3)
-					base_store_command[3] = (p  & 0xff).chr
+					base_store_command[3] = (p	& 0xff).chr
 					base_store_command[4] = (p >> 8 & 0xff).chr
 					base_store_command[13] = (bitmap_w & 0xff).chr
 					base_store_command[14] = (bitmap_w >> 8 & 0xff).chr
@@ -158,9 +157,13 @@ module Rescpos
 						commands << bitmap[pos].chr
 					end
 					commands << print_command
+					break
 				end
 			end
 			commands
 		end
-  end
+
+		def image2(image_file)
+		end
+	end
 end
